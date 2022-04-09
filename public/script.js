2afb241ffca8cf0b3230f15c07189519065eaca2
Icarus1989 <53438706+Icarus1989@users.noreply.github.com>
2022-04-09 18:15:40 +0200

    more testing

--- public/script.js
+++ public/script.js
@@ -1037,7 +1037,8 @@ window.addEventListener('resize', () => {
   // mainContainer.style.width = window.innerHeight + 'px';
   // document.documentElement.style.width = "100vh";
   // document.body.style.width = "100vh";
-  mainContainer.style.height = window.screen.height + 'px';
+  // mainContainer.style.height = window.screen.height + 'px';
+  mainContainer.style.height = (document.documentElement.offsetHeight || document.body.offsetHeight) + 'px'
   // mainContainer.style
   // mainContainer.style.height = (document.documentElement.scrollHeight || document.body.scrollHeight) + 'px';
 
