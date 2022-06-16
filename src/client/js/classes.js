16b905bb5a56a1aaa6427b9b9fffa4b7a6f4031f
Icarus1989 <53438706+Icarus1989@users.noreply.github.com>
2022-06-16 16:56:15 +0200

    more testing

--- src/client/js/classes.js
+++ src/client/js/classes.js
@@ -193,6 +193,7 @@ class Description {
       this.textbox = document.createElement('div');
       this.container.append(this.textbox);
     }
+    this.description.classList.add('descriptionBox>p');
     this.textbox.insertAdjacentHTML('afterbegin', this.description);
     this.textbox.classList.add('descriptionBox');
   }
