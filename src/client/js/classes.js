4a17aecb654a8b4c637eb80ac0697537c682791d
Icarus1989 <53438706+Icarus1989@users.noreply.github.com>
2022-06-17 09:34:54 +0200

    testing for bug fixing

--- src/client/js/classes.js
+++ src/client/js/classes.js
@@ -184,18 +184,17 @@ class Description {
   async createText() {
     this.textbox = document.querySelector('.descriptionBox');
     if (this.textbox) {
+      this.textbox.classList.add('descriptionBox');
       for (this.elem of this.textbox.querySelectorAll('p')) {
-        // console.log(this.elem);
         this.elem.remove();
       }
-      this.textbox.style.color = 'green';
-      this.textbox.classList.add('descriptionBox');
+      // this.textbox.style.color = 'green';
       // this.textbox.insertAdjacentHTML('afterbegin', this.description);
       // this.textbox.classList.add('descriptionBox');
     } else {
       this.textbox = document.createElement('div');
-      this.textbox.classList.add('descriptionBox');
       this.container.append(this.textbox);
+      this.textbox.classList.add('descriptionBox');
     }
     this.textbox.insertAdjacentHTML('afterbegin', this.description);
     // this.description.classList.add('.testOne');
